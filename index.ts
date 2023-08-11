d71ab80b69f19e06bb60bac5f66b774bffe4d96d
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-11 20:51:00 +0600

    model added and relation addeda

--- index.ts
+++ index.ts
@@ -7,14 +7,14 @@ async function main() {
   console.log(getAllUsers);
 
   //  crete data
-  const postUser = await prisma.user.create({
-    data: {
-      email: "nasirAhsan@gmail.com",
-      name: "nasir Ahsan",
-    },
-  });
+  // const postUser = await prisma.user.create({
+  //   data: {
+  //     email: "nasirAhsan@gmail.com",
+  //     name: "nasir Ahsan",
+  //   },
+  // });
 
-  console.log(postUser);
+  // console.log(postUser);
 }
 
 main();
