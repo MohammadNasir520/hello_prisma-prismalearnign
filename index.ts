9b9d2c7a013c7f71b04c40564330614d38a34dc2
MohammadNasir520 <nasirahsan520@gmail.com>
2023-08-11 16:30:12 +0600

    model created and also data save in database

---
+++ index.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+async function main() {
+  const getAllUsers = await prisma.user.findMany();
+  console.log(getAllUsers);
+
+  //  crete data
+  const postUser = await prisma.user.create({
+    data: {
+      email: "nasirAhsan@gmail.com",
+      name: "nasir Ahsan",
+    },
+  });
+
+  console.log(postUser);
+}
+
+main();
